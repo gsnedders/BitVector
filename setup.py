5728ea857ed7000d8c2b29b50bb78b5375ed8838
Geoffrey Sneddon <geoffers@gmail.com>
2015-07-07 17:31:17 +0100

    BitVector 2.0.1 release

--- setup.py
+++ setup.py
@@ -5,15 +5,21 @@
 from distutils.core import setup
 
 setup(name='BitVector',
-      version='2.0',
+      version='2.0.1',
       author='Avinash Kak',
       author_email='kak@purdue.edu',
       maintainer='Avinash Kak',
       maintainer_email='kak@purdue.edu',
-      url='http://RVL4.ecn.purdue.edu/~kak/dist/BitVector-2.0.html',
-      download_url='http://RVL4.ecn.purdue.edu/~kak/dist/BitVector-2.0.tar.gz?download',
+      url='http://RVL4.ecn.purdue.edu/~kak/dist/BitVector-2.0.1.html',
+      download_url='http://RVL4.ecn.purdue.edu/~kak/dist/BitVector-2.0.1.tar.gz?download',
       description='A pure-Python memory-efficient packed representation for bit arrays',
-      long_description='''This class presents a pure-Python memory efficient packed representation for bit arrays.
+      long_description='''
+This class presents a pure-Python memory efficient packed 
+representation for bit arrays.
+
+Version 2.0.1 removes many typos and other errors in the 
+documentation page.  The implementation code remains 
+the same as in Version 2.0
 
 Version 2.0 provides much additional functionality that was
 requested by folks in the data mining community.  The class
