fb65ebcd2eadac72e16fc3253d91535ddc6fb953
Geoffrey Sneddon <geoffers@gmail.com>
2015-07-07 17:30:31 +0100

    BitVector 1.5.1 release

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@
 from distutils.core import setup
 
 setup(name='BitVector',
-      version='1.5',
+      version='1.5.1',
       author='Avinash Kak',
       author_email='kak@purdue.edu',
       maintainer='Avinash Kak',
       maintainer_email='kak@purdue.edu',
-      url='http://RVL4.ecn.purdue.edu/~kak/dist/BitVector-1.5.html',
-      download_url='http://RVL4.ecn.purdue.edu/~kak/dist/BitVector-1.5.tar.gz?download',
+      url='http://RVL4.ecn.purdue.edu/~kak/dist/BitVector-1.5.1.html',
+      download_url='http://RVL4.ecn.purdue.edu/~kak/dist/BitVector-1.5.1.tar.gz?download',
       description='A pure-Python memory-efficient packed representation for bit arrays',
       long_description='''This class presents a pure-Python memory efficient packed representation for bit arrays.
 
+Version 1.5.1 removes a crucial bug in the right circular shift operator.
 
 Version 1.5 should prove to be much more efficient for long bit vectors.
 Efficiency in BitVector construction when only its size is specified was
@@ -25,11 +26,6 @@ version is the count_bits() method that returns the total number of bits
 set in a BitVector instance.  Yet another feature of this version is the
 setValue() method that alters the bit pattern associated with a
 previously constructed BitVector.
-
-In version 1.4.1, the reset() method now returns 'self' to allow for
-cascaded inovocation with the slicing operator.  This version also
-removes the discrepancy between the value of the  __copyright__ variable 
-in the module and the value of license variable in setup.py.
 
 The class supports the following operators/methods:
       
